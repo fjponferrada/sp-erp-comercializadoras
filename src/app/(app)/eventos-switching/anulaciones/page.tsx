6a50d6559a64a7f457eaa9b19679612cda643d50
unknown <Administrator@energia-aljaval.com>
2026-06-18 18:22:08 +0200

    style: remove padding from Generar Anulacion page wrapper

--- src/app/(app)/eventos-switching/anulaciones/page.tsx
+++ src/app/(app)/eventos-switching/anulaciones/page.tsx
@@ -2,9 +2,5 @@ import React from 'react';
 import GenerarAnulacionClient from './GenerarAnulacionClient';
 
 export default function GenerarAnulacionPage() {
-  return (
-    <div className="p-8 pb-20">
-      <GenerarAnulacionClient />
-    </div>
-  );
+  return <GenerarAnulacionClient />;
 }
