a0275215ef25e48a1dbbc70ff56ffd31937a5248
unknown <Administrator@energia-aljaval.com>
2026-07-28 22:14:43 +0200

    fix: provide dummy datasourceUrl for Vercel build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,7 +6,9 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL || "postgres://dummy:dummy@localhost:5432/dummy"
+  });
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
